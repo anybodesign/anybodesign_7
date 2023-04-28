b369613eb68e6b967a1adde90decd3dd781911e2
Thomas Villain <atelier@anybodesign.com>
2023-04-28 11:22:31 +0200

    page banner stain update

--- template-parts/page-banner-stain.php
+++ template-parts/page-banner-stain.php
@@ -1,5 +1,5 @@
 <?php if ( !defined('ABSPATH') ) die(); 
-	$img = get_the_post_thumbnail_url( $post->id, 'screen-hd' )
+	$img = get_the_post_thumbnail_url( $post->id, 'full' );
 ?>
 					<div class="featured-image-container">
 						<div class="featured-image" <?php echo 'style="background-image: url('.esc_url($img).');"'; ?>></div>
