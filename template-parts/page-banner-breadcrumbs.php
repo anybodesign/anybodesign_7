a80cff4c753fc1cbd9a06b9cbf9a04faafbdee6d
Thomas Villain <atelier@anybodesign.com>
2023-04-28 11:01:51 +0200

    breadcrumbs

--- template-parts/page-banner-breadcrumbs.php
+++ template-parts/page-banner-breadcrumbs.php
@@ -1,9 +1,63 @@
-<?php if ( !defined('ABSPATH') ) die(); ?>
+<?php if ( !defined('ABSPATH') ) die(); 
+	
+	$categories = get_the_category();
+	$terms = get_the_terms( $post->ID, 'type-creation' ); 
+?>
 					
-					<?php if ( function_exists('bcn_display') ) { ?>
-					<div class="breadcrumbs-nav">
-						<nav class="inner">
-							<?php bcn_display(); ?>					
-						</nav>
-					</div>
-					<?php } ?>						
+							<nav class="breadcrumbs-nav" role="navigation" aria-label="<?php esc_attr_e('Breadcrumbs', 'anybodesign'); ?>">
+								<ul class="cat-menu">
+									
+								<?php if (is_singular('post')) { ?> 
+									
+									<li class="cat-item">
+										<?php esc_html_e('Posted on ','anybodesign'); the_time('d.m.Y');  esc_html_e(' in','anybodesign'); ?>
+									</li>
+									
+									<li class="cat-item">
+										<a href="<?php echo esc_url( home_url( '/blog/' ) ); ?>">
+											Blog
+										</a>
+									</li>
+									<?php if ( ! empty( $categories ) ) { ?>
+									<li class="cat-item">
+										<a href="<?php echo esc_url( get_category_link( $categories[0]->term_id ) ); ?>">
+											<?php echo esc_html( $categories[0]->name ); ?>
+										</a>
+									</li>
+									<?php } ?>
+									
+								<?php } ?>
+									
+									
+								<?php if (is_singular('projet')) { ?> 
+									
+									<li class="cat-item">
+										<?php esc_html_e('Posted in','anybodesign'); ?>
+									</li>
+										
+										<?php
+										if ( $terms && ! is_wp_error( $terms ) ) : 
+											$taxo_links = array();
+												foreach ( $terms as $term ) {
+													$taxo_links[] = '<a href="'.esc_url( home_url( '/' ) ).'portfolio-anybodesign/#'.$term->slug.'">'.$term->name.'</a>';
+												}
+											$taxlinks = join( " — ", $taxo_links );
+										endif; ?>										
+									
+									<li class="cat-item">
+										<a href="<?php echo esc_url( home_url( '/portfolio-anybodesign/' ) ); ?>">Portfolio</a>
+									</li>
+									<?php if ( has_term('archives', 'type-creation') ) { ?>
+									<li class="cat-item">
+										<a href="<?php echo esc_url( home_url( '/creations/archives/' ) ); ?>">Archives</a>
+									</li>
+									<?php } else { ?>
+									<li class="cat-item">
+										<?php echo $taxlinks; ?>
+									</li>
+									<?php } ?>
+									
+								<?php } ?>
+									
+								</ul>
+							</nav>
