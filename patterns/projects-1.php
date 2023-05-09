170db46aedec5c19c9b956bc1dab0b0d214db460
Thomas Villain <atelier@anybodesign.com>
2023-05-09 12:09:19 +0200

    project patterns

---
+++ patterns/projects-1.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Title: Project #1
+ * Slug: anybodesign/projects-1
+ * Categories: featured
+ * Keywords: Projects, projets
+ * Block Types: core/image
+ */
+?>
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group">
+	<!-- wp:image {"sizeSlug":"large","linkDestination":"media"} -->
+	<figure class="wp-block-image size-large">
+		<a href="<?php echo esc_url( FS_THEME_URL . '/patterns/assets/anybodesign-placeholder.png'); ?>">
+			<img src="<?php echo esc_url( FS_THEME_URL . '/patterns/assets/anybodesign-placeholder.png'); ?>" alt=""/>
+		</a>
+	</figure>
+	<!-- /wp:image -->
+	
+	<!-- wp:columns -->
+	<div class="wp-block-columns">
+		<!-- wp:column -->
+		<div class="wp-block-column">
+			<!-- wp:image {"sizeSlug":"large","linkDestination":"media"} -->
+			<figure class="wp-block-image size-large">
+				<a href="<?php echo esc_url( FS_THEME_URL . '/patterns/assets/anybodesign-placeholder.png'); ?>">
+					<img src="<?php echo esc_url( FS_THEME_URL . '/patterns/assets/anybodesign-placeholder.png'); ?>" alt=""/>
+				</a>
+			</figure>
+			<!-- /wp:image -->
+		</div>
+		<!-- /wp:column -->
+		
+		<!-- wp:column -->
+		<div class="wp-block-column">
+			<!-- wp:image {"sizeSlug":"large","linkDestination":"media"} -->
+			<figure class="wp-block-image size-large">
+				<a href="<?php echo esc_url( FS_THEME_URL . '/patterns/assets/anybodesign-placeholder.png'); ?>">
+					<img src="<?php echo esc_url( FS_THEME_URL . '/patterns/assets/anybodesign-placeholder.png'); ?>" alt=""/>
+				</a>
+			</figure>
+			<!-- /wp:image -->
+		</div>
+		<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
+
+</div>
+<!-- /wp:group -->
