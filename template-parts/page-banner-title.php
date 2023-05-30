52532588ec06fe2d484d0ad5deb8e91dc3077864
Thomas Villain <atelier@anybodesign.com>
2023-05-30 23:57:49 +0200

    search title

--- template-parts/page-banner-title.php
+++ template-parts/page-banner-title.php
@@ -7,7 +7,7 @@
 								} else if ( is_archive() ) {
 									the_archive_title();
 								} else if ( is_search() ) {
-									printf( esc_html__( 'Search Results for: %s', 'anybodesign' ), '<span class="search-term">' . get_search_query() . '</span>' );
+									printf( esc_html__( 'Your search results for: %s', 'anybodesign' ), '<span class="search-term">' . get_search_query() . '</span>' );
 								} else if ( is_404() ) {
 									esc_html_e( 'Oops! That page can&rsquo;t be found', 'anybodesign' );
 								} else {
