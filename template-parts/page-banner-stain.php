a60828d8616f0fc655e8d1bd98273b2695b5f681
Thomas Villain <atelier@anybodesign.com>
2023-05-31 15:32:31 +0200

    featured image alt

--- template-parts/page-banner-stain.php
+++ template-parts/page-banner-stain.php
@@ -1,6 +1,15 @@
 <?php if ( !defined('ABSPATH') ) die(); 
 	$img = get_the_post_thumbnail_url( $post->id, 'full' );
+	$img_alt = get_field('featured_alt');
+	
+	if ($img_alt) {
+		$bg = 'style="background-image: url('.esc_url($img_alt).');"';
+	} else if ($img) {
+		$bg = 'style="background-image: url('.esc_url($img).');"';
+	} else {
+		$bg = null;
+	}
 ?>
 					<div class="featured-image-container">
-						<div class="featured-image" <?php echo 'style="background-image: url('.esc_url($img).');"'; ?>></div>
+						<div class="featured-image" <?php echo $bg; ?>></div>
 					</div>
